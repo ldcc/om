5b6bcf5eba05ac61c1db2d3fce804a8985eeb2d2
ldcc <eldhrimnirs@gmail.com>
2017-04-19 12:17:53 +0800

    Annotating DBManager

--- src/com/ldcstorm/applet/om2/dbm/DBManager.java
+++ src/com/ldcstorm/applet/om2/dbm/DBManager.java
@@ -9,10 +9,16 @@ import java.sql.Statement;
 import java.util.List;
 
 public class DBManager {
+    /**
+     * Database parameters for initialize your database
+     */
     protected static final String DBURL = "jdbc:mysql://localhost:9633/30561dbCsjza";
     protected static final String USERNAME = "30561_fH8714";
     protected static final String PASSWORD = "kDTHq98xE3bcjkZ";
 
+    /**
+     * As you can see, dbManager is a singleton object
+     */
     private DBManager() {
         try {
             Class.forName("com.mysql.jdbc.Driver").newInstance();
@@ -27,17 +33,13 @@ public class DBManager {
         return dbManager;
     }
 
-
-    /**
-     * @param dtm A tool for building object type of O
-     * @param <O> As you can see, O extend from DTO
-     */
-
     /**
      * Getting object with single column and value
      *
+     * @param dtm    A tool for building object type of O
      * @param column A column name from your database
      * @param value  A data value from that column
+     * @param <O>    As you can see, O extend from DTO
      * @return Object cast to O and then return that
      */
     public <O extends DTO> O getObj(DTM<O> dtm, String column, String value) {
@@ -54,6 +56,8 @@ public class DBManager {
     /**
      * Getting All object
      *
+     * @param dtm A tool for building object type of O
+     * @param <O> As you can see, O extend from DTO
      * @return All Object will add to List<O>, but no filter
      */
     public <O extends DTO> List<O> getAll(DTM<O> dtm) {
@@ -68,12 +72,12 @@ public class DBManager {
     }
 
     /**
-     * @param o just a object you wanted to do something
-     * @return return the action whether success
-     */
-
-    /**
      * Insert o under beneath the last row with dtm from database
+     *
+     * @param dtm A tool for building object type of O
+     * @param o   just a object you wanted to do something
+     * @param <O> As you can see, O extend from DTO
+     * @return return the action whether success
      */
     public <O extends DTO> boolean addObj(DTM<O> dtm, O o) {
         try (Connection conn = DriverManager.getConnection(DBURL, USERNAME, PASSWORD)) {
@@ -88,6 +92,11 @@ public class DBManager {
 
     /**
      * update o with dtm from database
+     *
+     * @param dtm A tool for building object type of O
+     * @param o   just a object you wanted to do something
+     * @param <O> As you can see, O extend from DTO
+     * @return return the action whether success
      */
     public <O extends DTO> boolean update(DTM<O> dtm, O o) {
         try (Connection conn = DriverManager.getConnection(DBURL, USERNAME, PASSWORD)) {
@@ -102,6 +111,11 @@ public class DBManager {
 
     /**
      * delete o(a whole row) with dtm from database
+     *
+     * @param dtm A tool for building object type of O
+     * @param o   just a object you wanted to do something
+     * @param <O> As you can see, O extend from DTO
+     * @return return the action whether success
      */
     public <O extends DTO> boolean delete(DTM<O> dtm, O o) {
         try (Connection conn = DriverManager.getConnection(DBURL, USERNAME, PASSWORD)) {
